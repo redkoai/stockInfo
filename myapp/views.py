d1bb55c76d0ca70398bfb59054d7b89f074d0b0d
redkoai <redko@theredko.com>
2023-07-14 10:55:59 -0700

    apps.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-
 from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
+@require_POST
 def get_stock_info(request):
     ticker = request.POST.get('ticker')
-    # Add your code to fetch stock information using yfinance
-    # and return the desired JSON response
+    stock = yf.Ticker(ticker)
+    info = stock.info
     data = {
         'ticker': ticker,
+        'info': stock.info,
+       
     }
     return JsonResponse(data)
-
